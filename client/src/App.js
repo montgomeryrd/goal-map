3dd795ef4d527f3391d3ae480a59f82646e15b25
montgomeryrd <montgomery_rd@yahoo.com>
2020-12-26 22:34:52 -0800

    implemented material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { AppBar, Container, Grid, Grow, Typography } from '@material-ui/core';
+import Posts from './components/posts/Posts';
+import Form from './components/form/Form';
 
 function App() {
   return (
-    <div className="App">
-      <h1>butt</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="left">Growth</Typography>
+        {/* <img src={logo} alt="growth" height="60" /> */}
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing="3">
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,14 @@
+import Post from './post/Post';
+
+const Posts = () => {
+    return (
+        <div>
+           posts 
+           <Post>
+               
+           </Post>
+        </div>
+    )
+}
+
+export default Posts;
